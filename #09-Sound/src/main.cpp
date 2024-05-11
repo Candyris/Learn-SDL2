6eaae11eb29a84bd1a9badb39bdf90ba317399af
Candyrizz <rishavsingh076@gmail.com>
2024-05-11 23:23:43 +0530

    Update main.cpp

--- #09-Sound/src/main.cpp
+++ #09-Sound/src/main.cpp
@@ -1,1 +1,45 @@
-k
+// for simplist i will be using SDL2 default music handler
+// if You are making big game you should use SDL_Mixer
+
+#include <SDL2/SDL.h>
+#include <iostream>
+
+#define SOUNDFILEPATH "sound.wav"
+
+int main(int argc, char **argv)
+{
+    if (SDL_Init(SDL_INIT_AUDIO) != 0)
+    {
+        std::cout << "FAILED  INITIALIZE SDL2 AUDIO \n";
+        SDL_Quit();
+        return -1;
+    }
+    else
+    {
+        std::cout << "[Successful in INITALIZE SDL2 AUIDO] \n\n";
+    }
+    std::cout << "Hello" << std::endl;
+    // Create Audio stuff for using Audio
+    SDL_AudioSpec wavSpec; // it help us to describe audio format
+    Uint32 wavLength;      // this is length of Audio Buffer in / convert into Bytes
+    Uint8 *wavBuffer;      // this will be containing the audio data buffer
+
+    // Loading WAV File into program
+    SDL_LoadWAV(SOUNDFILEPATH, &wavSpec, &wavBuffer, &wavLength);
+
+    // Open audio Device
+    SDL_AudioDeviceID deviceId = SDL_OpenAudioDevice(NULL, NULL, &wavSpec, NULL, NULL);
+    // play audio
+
+    int success = SDL_QueueAudio(deviceId, wavBuffer, wavLength);
+    SDL_PauseAudioDevice(deviceId, 0);
+    // keep application running long enough to hear the sound
+
+    SDL_Delay(3000);
+
+    SDL_CloseAudioDevice(deviceId);
+    SDL_FreeWAV(wavBuffer);
+    SDL_Quit();
+
+    return 0;
+}
