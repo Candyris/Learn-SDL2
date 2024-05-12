d4f13b49df7cb1cb6ab6472bb6859b04685767f3
Candyrizz <129657681+Candyris@users.noreply.github.com>
2024-05-13 01:47:29 +0530

    Update Sprite.h

--- #10-SpriteSheet/src/Sprite.h
+++ #10-SpriteSheet/src/Sprite.h
@@ -3,19 +3,24 @@
 
 class Vector2i {
 public:
-	Vector2i(int p_X ,int p_Y) : x(p_X) , y (p_Y){}
 	int x, y;
+	Vector2i() { x =  y = 0; }
+	Vector2i(int p_X = 0 ,int p_Y = 0) : x(p_X) , y (p_Y){}
 };
 
 class Sprite
 {
 public:
 	Sprite();
-	Sprite(const char* filePath,Vector2i srcSpritePosition, Vector2i dsntSpritePosition);
+	Sprite(SDL_Renderer* p_Renderer,const char* filePath,Vector2i srcSpritePosition, Vector2i dsntSpritePosition);
 	//Sprite(const char* filePath, Vector2i srcPosition, Vector2i dsntPosition);
 	~Sprite();
 	
+	virtual void setSourceImageSize(Vector2i p_SrcImgPos);
+	virtual void setSourceImagePosition(Vector2i p_SrcImgSize);
+
 	// setters
+	void setRenderer(SDL_Renderer* p_Renderer);
 	void setPosition(Vector2i p_Position);
 	void setSize(Vector2i p_Size);
 	void loadTexture(const char* p_FilePath);
@@ -27,11 +32,12 @@ public:
 	virtual void update(int p_DeltaTime) = 0;
 	virtual void render();
 protected:
+	SDL_Renderer* m_Renderer;
 	SDL_Texture* m_SpriteTexture;
 	SDL_RendererFlip m_SpriteFlip;
 	int m_SpriteAngle;
+	SDL_Rect m_SpritedsntRect;
 private:
 	SDL_Rect m_SpriteSrcRect;
-	SDL_Rect m_SpritedsntRect;
 };
 
