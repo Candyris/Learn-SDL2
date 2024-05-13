5518a8128e4f416294048152f23a49bb0d154907
Candyrizz <129657681+Candyris@users.noreply.github.com>
2024-05-14 00:40:44 +0530

    Update SpriteSheet.h

--- #10-SpriteSheet/src/Sprite/SpriteSheet.h
+++ #10-SpriteSheet/src/Sprite/SpriteSheet.h
@@ -1,17 +1,19 @@
 #pragma once 
 #include <Vector>
 #include <SDL2/SDL.h>
-#include "Sprite.h"
+#include "../Sprite/Sprite.h"
+
 class SpriteSheet : public Sprite {
 public:
-	SpriteSheet();
-	void setSourceImageSize(Vector2i p_SrcImgPos) override;
-	void setSourceImagePosition(Vector2i p_SrcImgSize) override;
-	void setSpriteSheetDelay(int p_MilliSecond);
-	void update(int p_DeltaTime) override;
+    SpriteSheet(SDL_Renderer* p_Renderer, const char* p_FilePath, Vector2i p_SrcSpritePosition, Vector2i p_SrcSpriteSize, int p_NumRows, int p_NumColumns);
+    void setSpriteSheetDelay(int p_MilliSecond);
+    void update(int p_DeltaTime) override;
+    void render() override;
 protected:
-	std::vector<SDL_Rect> m_SpriteSheetDsntRect;
-  int m_CurrentSpriteIndex; // Index of the current sprite being displayed
+    std::vector<SDL_Rect> m_SpriteSheetDsntRect;
+    int m_CurrentSpriteIndex; // Index of the current sprite being displayed
     int m_SpriteSheetDelay; // Delay between sprite changes in milliseconds
     int m_TimeSinceLastSpriteChange; // Time elapsed since last sprite change
+    int m_NumRows; // Number of rows in the sprite sheet
+    int m_NumColumns; // Number of columns in the sprite sheet
 };
